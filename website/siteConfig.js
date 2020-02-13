6dc79873509488d37ad6e20ff9e76dc366c1b489
ANN <53808614+ANNMARIA1996@users.noreply.github.com>
2020-02-13 17:32:29 +0530

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -42,7 +42,7 @@ const siteConfig = {
     {doc: 'doc4', label: 'Doc'},
     {page: 'help', label: 'Help'},
     {blog: true, label: 'contact'},
-    {page:'./website/core/savePDF.js', label: 'Save PDF'},
+    {nodejs:'./website/core/savePDF.js', label: 'Save PDF'},
     //   {href: 'https://v2.convertapi.com/convert/web/to/pdf?secret=&download=attachment&url=' + encodeURI(window.location), label: 'PDF'},
   ],
 
