1ab7be86e19ccec9c7f18e65f964fd4da71cc850
ANN <53808614+ANNMARIA1996@users.noreply.github.com>
2020-02-13 16:34:55 +0530

    Update Footer.js

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -6,22 +6,7 @@
  */
 
 const React = require('react');
-
-class Footer extends React.Component {
-  docUrl(doc, language) {
-    const baseUrl = this.props.config.baseUrl;
-    const docsUrl = this.props.config.docsUrl;
-    const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
-    const langPart = `${language ? `${language}/` : ''}`;
-    return `${baseUrl}${docsPart}${langPart}${doc}`;
-  }
-
-  pageUrl(doc, language) {
-    const baseUrl = this.props.config.baseUrl;
-    return baseUrl + (language ? `${language}/` : '') + doc;
-  }
-	
-	printPDF.then(pdf => {
+printPDF.then(pdf => {
 	res.set({ 'Content-Type': 'application/pdf', 'Content-Length': pdf.length })
 	res.send(pdf)
   function getPDF() {
@@ -44,6 +29,23 @@ class Footer extends React.Component {
      })
    .catch(err => /** error handling **/)
  }
+	  
+	  
+class Footer extends React.Component {
+  docUrl(doc, language) {
+    const baseUrl = this.props.config.baseUrl;
+    const docsUrl = this.props.config.docsUrl;
+    const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
+    const langPart = `${language ? `${language}/` : ''}`;
+    return `${baseUrl}${docsPart}${langPart}${doc}`;
+  }
+
+  pageUrl(doc, language) {
+    const baseUrl = this.props.config.baseUrl;
+    return baseUrl + (language ? `${language}/` : '') + doc;
+  }
+	
+	
 	  
 <button onClick={this.savePDF}>Save as PDF</button>  
   render() {
