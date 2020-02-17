e6b6b8ae4db39854c457fbb1ee0f077125ca938f
ANN <53808614+ANNMARIA1996@users.noreply.github.com>
2020-02-17 12:03:07 +0530

    Update index.js

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -213,7 +213,18 @@ class Index extends React.Component {
   }
 }
 
-
-
+var markdownpdf = require("markdown-pdf")
+var options = {
+    remarkable: {
+        html: true,
+        breaks: true,
+        plugins: [ require('remarkable-classy') ],
+        syntax: [ 'footnote', 'sup', 'sub' ]
+    }
+}
+ 
+markdownpdf(options)
+  .from("/path/to/document.md")
+  .to("/path/to/document.pdf", function () { console.log("Done") })
   
 module.exports = Index;
